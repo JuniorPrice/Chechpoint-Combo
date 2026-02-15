45cbf2d08ba1b46a066592f9cd12fa60d72d9f75
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-02-15 16:22:48 +0400

    prepare basic structure

---
+++ Ticket_CLI.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ticket_CLI {
+    public static void main(String[] args) {
+
+        // declear variables
+        ArrayList<String> tickets = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        String ticket = "";
+        
+
+        // declear git index function
+
+
+        // declear createTicket function
+
+
+        // declear listTickets function
+
+
+        // declear updateTicket function
+
+
+        // declear deleteTicket function
+
+        
+
+    }
+    
+}
