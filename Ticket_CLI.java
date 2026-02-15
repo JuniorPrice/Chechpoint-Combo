832c71699c45a5b87343c4ebcff6e0b98595b1c4
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-02-15 23:18:26 +0400

    Add git index function and set adding ticket functionality

--- Ticket_CLI.java
+++ Ticket_CLI.java
@@ -15,28 +15,36 @@ public class Ticket_CLI {
         System.out.println("               Welcom to Ticket Desk CLI");
         System.out.println("=".repeat(50));
         System.out.println("Available Servecies:");
-        System.out.println("(1) to Create a ticket\n(2) to List all tickets\n(3) to Update a ticket\n(4) to Close/Delete a ticket\n(5) to Exit\n");
+        System.out.println("(1) to Create a ticket\n(2) to List all tickets\n(3) to Update a ticket\n(4) to Close/Delete a ticket\n(5) to Exit");
         
 
 
         // procces user choice
 
         while(count){
-            System.out.print(">> Choose your service: ");
+            System.out.print("\n>> Choose your service: ");
             int choice = input.nextInt();
             if (choice > 0 && choice < 6){
 
                 if (choice == 1){
-                    // createTicket();
+                    System.out.print("\nEnter your new ticket: ");
+                    String ticket = input.next();
+                    if (gitIndex(tickets,ticket) != -1){
+                        System.out.println("This ticket already exists!!");
+                    }
+                    else{
+                        tickets.add(ticket);
+                        System.out.println("Congrats! Your ticket has been added ...");
+                    }
                 }
                 else if (choice == 2){
-                    // listTickets();
+                    //declear listTickets function
                 }
                 else if (choice == 3){
-                    // updateTicket();
+                    // declear updateTicket function
                 }
                 else if (choice == 4){
-                    // deleteTicket();
+                    // declear deleteTicket function
                 }
                 else if (choice == 5){
                     System.out.println("\nThank you for your time!...\n");
@@ -53,19 +61,18 @@ public class Ticket_CLI {
     }
 
     // declear gitIndex function
+    static int gitIndex(ArrayList<String> tickets,String ticket){
+        int index = -1;
+        for (int i=0 ; i<tickets.size() ; i++){
+            if (tickets.get(i).equals(ticket)){
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
     
-
-
-    // declear createTicket function
-
-
-    // declear listTickets function
-
-
-    // declear updateTicket function
-
-
-    // declear deleteTicket function
 
     
 }
